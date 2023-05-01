b686c7b71e00fdd64a3d41bd612112e23c574a2c
Roman Kopylov <romankopilov1712@gmail.com>
2023-05-01 12:59:50 +0300

    configured swiper

--- js/main.js
+++ js/main.js
@@ -1,31 +1,10 @@
 import '../style_01_block.css'
 import addSwitchingImages from './modules/images';
-import Swiper from 'swiper/bundle';
-import 'swiper/css/bundle';
+import swiper from './modules/swiper';
 
 document.addEventListener('DOMContentLoaded', () => {
 
     addSwitchingImages();
 
-    const swiper = new Swiper('.swiper', {
-        slidesPerView: 3,
 
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        },
-        pagination: {
-            el: '.swiper-pagination',
-            // type: 'progressbar',
-            clickable: true,
-            dynamicBullets: true,
-        },
-        renderBullets: (index, className) => {
-            return '<span class="' + className + '">' + (index + 1) + '</span>';
-        },
-        keyboard: {
-            enabled: true,
-            onlyInViewport: 3
-        },
-    });
 });
---
+++ js/modules/swiper.js
@@ -0,0 +1,47 @@
+import Swiper from 'swiper/bundle';
+import 'swiper/css/bundle';
+
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3,
+    centeredSlides: true,
+    loop: true,//должно быть минимум 6 слайдов для корректной работы
+    spaceBetween: 10,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        // type: 'progressbar',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: 3
+    },
+    autoplay: {
+        delay: 2000
+    },
+    effect: 'coverflow',
+    coverflowEffect: {
+        rotate: 30,
+        slideShadows: false,
+    },
+    breakpoints: {
+        300: {
+            slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+        },
+        800: {
+            slidesPerView: 3,
+        },
+        1300: {
+            slidesPerView: 4,
+        }
+    },
+});
+
+export default swiper;
