77641b7b55dc784515eca28aabbf6c2d3d8bde09
Roman Kopylov <romankopilov1712@gmail.com>
2023-06-02 11:53:10 +0300

    changes to layout

--- js/modules/map.js
+++ js/modules/map.js
@@ -1,39 +1,38 @@
 const createMap = () => {
     const map = document.querySelector('#yamap');
     const center = [53.211781, 34.303434];
-
-    ymaps.ready(function() {
+    //create map
+    ymaps.ready(function () {
         const myMap = new ymaps.Map(map, {
             center: center,
-            zoom:16,
+            zoom: 16,
             controls: ['routePanelControl']
         });
-
-        const placemark = new ymaps.Placemark(center,{
+        //add placemark
+        const placemark = new ymaps.Placemark(center, {
             balloonContentHeader: 'Мак Мебель',
             balloonContentBody: 'ул. Шоссейная, д.32',
             balloonContentFooter: '+7 910 333 38 30\n' +
                 '+7 910 330 99 29'
-        }, {
+        }, { // change default icon
             iconLayout: "default#image",
-        iconImageHref: '/free-icon-factory-7775060.png',
-        iconImageSize: [20,20],
-            iconImageOffset: [-20,0]
+            iconImageHref: '/free-icon-factory-7775060.png',
+            iconImageSize: [20, 20],
+            iconImageOffset: [-20, 15]
         });
         myMap.geoObjects.add(placemark);
-
+        //get location
         const location = ymaps.geolocation.get();//promise
-        location.then(function(result) {
-const locationText = result.geoObjects.get(0).properties.get('text');
-
-            const control = myMap.controls.get('routePanelControl');
-
-            control.routePanel.state.set({
+        location.then(function (result) {
+            const locationText = result.geoObjects.get(0).properties.get('text');
+            //set route
+            const routePanelControl = myMap.controls.get('routePanelControl');
+            routePanelControl.routePanel.state.set({
                 type: 'masstransit',
                 from: locationText,
                 toEnabled: false,//нельзя заполнять поле куда
                 to: center
-        });
+            });
         })
     });
 }
