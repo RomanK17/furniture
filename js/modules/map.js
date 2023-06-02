c517aa1da49277ad0251954b6180a39b86bf80a5
Roman Kopylov <romankopilov1712@gmail.com>
2023-06-02 07:59:52 +0300

    add geolocation

--- js/modules/map.js
+++ js/modules/map.js
@@ -3,17 +3,30 @@ const createMap = () => {
     const center = [53.211781, 34.303434];
 
     ymaps.ready(function() {
-        let myMap = new ymaps.Map(map, {
+        const myMap = new ymaps.Map(map, {
             center: center,
             zoom:16
         });
 
-        let placemark = new ymaps.Placemark(center,{}, {iconLayout: "default#image",
+        const placemark = new ymaps.Placemark(center,{
+            balloonContentHeader: 'Мак Мебель',
+            balloonContentBody: 'ул. Шоссейная, д.32',
+            balloonContentFooter: '+7 910 333 38 30\n' +
+                '+7 910 330 99 29'
+        }, {
+            iconLayout: "default#image",
         iconImageHref: '/free-icon-factory-7775060.png',
         iconImageSize: [20,20],
             iconImageOffset: [-20,0]
         });
         myMap.geoObjects.add(placemark);
+
+        const location = ymaps.geolocation.get();//promise
+
+        location.then(function(result) {
+const locationText = result.geoObjects.get(0).properties.get('text');
+console.log(locationText);
+        });
     });
 }
 
